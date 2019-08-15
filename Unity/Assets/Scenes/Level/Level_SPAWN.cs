029c14dd62fdccadb0d49cf31e660079722028ea
Polaris1080 <Polaris1080@outlook.jp>
2019-08-15 20:00:16 +0900

    Level_SPAWN

--- Unity/Assets/Scenes/Level/Level_SPAWN.cs
+++ Unity/Assets/Scenes/Level/Level_SPAWN.cs
@@ -14,71 +14,49 @@ public class Level_SPAWN : MonoBehaviour{
     float span = 0; //スポーンさせる間隔
 
 
-    /*
-    public void Update()
-    {
-        float span = 0; //スポーンさせる間隔
-        this.delda_spawn += Time.deltaTime; //デルタ秒を更新
-        //int blocks = GameObject.FindGameObjectsWithTag("Block").Length; //Level内に存在するBlockを数える
-        
-        /*
-        bool[] T =new bool[6];  //先に条件式を計算しておく
-        T[0] =                             0 <= blocks;    T[1] =  blocks < this.Header.threshold_spawn.x;
-        T[2] = this.Header.threshold_spawn.x <= blocks;    T[3] =  blocks < this.Header.threshold_spawn.y;
-        T[4] = this.Header.threshold_spawn.y <= blocks;    T[5] =  blocks < this.Header.threshold_spawn.z;
-            
-
-        //Blockの数に合わせて、スポーン間隔を調整
-        if      ( T[0] && T[1] ) { span = this.Header.span_spawn.x; }
-        else if ( T[2] && T[3] ) { span = this.Header.span_spawn.y; }
-        else if ( T[4] && T[5] ) { span = this.Header.span_spawn.z; }
-        else                     { span = Mathf.Infinity; }
-        
-        if (this.delda_spawn > span) //スポーン出来るだけの間隔が空いたら
-        {
-            this.delda_spawn = 0; //積算デルタ秒を初期化
-
-            int x = UnityEngine.Random.Range(this.Header.range_spawn.x/(-2), this.Header.range_spawn.x/(2))*2;
-            int y = UnityEngine.Random.Range(0, this.Header.range_spawn.y+1)*2;
-            Vector2Int z = new Vector2Int(x,y);
-
-            if (memory.Contains(z)) //スポーン履歴を確認
-            {
-                while (memory.Count > this.Header.memory_spawn) { memory.Dequeue(); } //上限を超えているなら削除
-            }
-            else
-            {
-                memory.Enqueue(z); //履歴に記録
-                GameObject item = Instantiate(this.Header.spawntarget) as GameObject; //スポーン
-                item.transform.position = new Vector3(x, y, 0); //位置調整
-            }
-        }
-    }
-    */
-
     public void Start()
     {
-        int blocks = GameObject.FindGameObjectsWithTag("Block").Length;
-        Subject<bool> boolsubject = new Subject<bool>();
-        var disposable1 = boolsubject.Subscribe(x => {
-            T[0] = 0 <= blocks; T[1] = blocks < this.Header.threshold_spawn.x;
-            T[2] = this.Header.threshold_spawn.x <= blocks; T[3] = blocks < this.Header.threshold_spawn.y;
-            T[4] = this.Header.threshold_spawn.y <= blocks; T[5] = blocks < this.Header.threshold_spawn.z;
-        });
-        var disposable2 = boolsubject.Subscribe(x => {
-            if (T[0] && T[1]) { span = this.Header.span_spawn.x; }
-            else if (T[2] && T[3]) { span = this.Header.span_spawn.y; }
-            else if (T[4] && T[5]) { span = this.Header.span_spawn.z; }
-            else { span = Mathf.Infinity; }
-        });
-        var disposable3 = boolsubject.Subscribe(x => {
-            Debug.Log("D"); 
-        });
+        
 
-        this.FixedUpdateAsObservable()
-            .Subscribe(_ => Debug.Log("Update!"));
+
+
+        //スポーン出来るだけの間隔が空いたら
         Observable.EveryUpdate()
-                  .Subscribe(_ => { Debug.Log("D"); })
+                  .Where(_ => this.delda_spawn > CalculateSpan())
+                  .Subscribe(_ => {
+                      this.delda_spawn = 0; //積算デルタ秒を初期化
+
+                      int x = UnityEngine.Random.Range(this.Header.range_spawn.x / (-2), this.Header.range_spawn.x / (2)) * 2;
+                      int y = UnityEngine.Random.Range(0, this.Header.range_spawn.y + 1) * 2;
+                      Vector2Int z = new Vector2Int(x, y);
+
+                      if (memory.Contains(z)) //スポーン履歴を確認
+                      {
+                          while (memory.Count > this.Header.memory_spawn) { memory.Dequeue(); } //上限を超えているなら削除
+                      }
+                      else
+                      {
+                          memory.Enqueue(z); //履歴に記録
+                          GameObject item = Instantiate(this.Header.spawntarget) as GameObject; //スポーン
+                          item.transform.position = new Vector3(x, y, 0); //位置調整
+                      }
+                  })
                   .AddTo(this.gameObject);
+    }
+
+    private float CalculateSpan() {
+        int blocks = GameObject.FindGameObjectsWithTag("Block").Length;//Level内に存在するBlockを数える
+        this.delda_spawn += Time.deltaTime; //デルタ秒を更新
+        T[0] = 0 <= blocks; T[1] = blocks < this.Header.threshold_spawn.x;
+        T[2] = this.Header.threshold_spawn.x <= blocks; T[3] = blocks < this.Header.threshold_spawn.y;
+        T[4] = this.Header.threshold_spawn.y <= blocks; T[5] = blocks < this.Header.threshold_spawn.z;
+
+        //Blockの数に合わせて、スポーン間隔を調整
+        if (T[0] && T[1]) { span = this.Header.span_spawn.x; }
+        else if (T[2] && T[3]) { span = this.Header.span_spawn.y; }
+        else if (T[4] && T[5]) { span = this.Header.span_spawn.z; }
+        else { span = Mathf.Infinity; }
+
+        return span;
     }
 }
